5810479fb3549218fd7fc8d2c349e27477dd67ee
Machiastle <92347210+Machiastle@users.noreply.github.com>
2024-06-05 16:41:30 +0800

    Update login_header.php

--- include/login_header.php
+++ include/login_header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/bootstrap.theme.min.css">
-  <link rel="stylesheet" href="assets/css/custom.css">
+  <link rel="stylesheet" href="assets/css/customs.css">
   <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
   <script src="assets/js/custom.js"></script>
